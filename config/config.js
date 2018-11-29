0f17aff545bd1a8bee5410a202689e0c34d94433
jrf <yyn@126mail.cn>
2018-11-29 18:39:52 +0800

    搭建基于model的卡片列表：1

--- config/config.js
+++ config/config.js
@@ -1,30 +1,32 @@
 export default {
-  singular: true,
-  plugins: [
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true
-    }]
-  ],
-  routes: [{
-    path: '/',
-    component: '../layout',
-    routes: [{
-      path: '/',
-      component: './HelloWorld',
-    }, {
-      path: '/helloWorld',
-      component: './HelloWorld'
-    }, {
-      path: '/card',
-      component: './Card'
-    }, {
-      path: '/dashboard',
-      routes: [
-        {path: '/dashboard/analysis', component: 'Dashboard/Analysis'},
-        {path: '/dashboard/monitor', component: 'Dashboard/Monitor'},
-        {path: '/dashboard/workplace', component: 'Dashboard/Workplace'}
-      ]
-    }]
-  }],
+	singular: true,
+	plugins: [
+		['umi-plugin-react', {
+			antd: true,
+			dva: true
+		}]
+	],
+	routes: [{
+		path: '/',
+		component: '../layout',
+		routes: [{
+			path: '/',
+			component: './HelloWorld',
+		}, {
+			path: '/helloWorld',
+			component: './HelloWorld'
+		}, {
+			path: '/card',
+			component: './Card'
+		}, {
+			path: 'puzzlecards', component: './puzzlecards'
+		}, {
+			path: '/dashboard',
+			routes: [
+				{path: '/dashboard/analysis', component: 'Dashboard/Analysis'},
+				{path: '/dashboard/monitor', component: 'Dashboard/Monitor'},
+				{path: '/dashboard/workplace', component: 'Dashboard/Workplace'}
+			]
+		}]
+	}],
 };
--- src/page/Dashboard/Analysis.js
+++ src/page/Dashboard/Analysis.js
@@ -1,15 +1,18 @@
 import {Component} from "react";
 
 export default class Analysis extends Component {
-    componentDidMount() {
-        console.log('didMount Analysis: 挂载后');
-    }
-    componentWillUnmount() {
-        console.log('willUnmount Analysis: 卸载前');
-    }
+	componentDidMount() {
+		console.log('didMount Analysis: 挂载后');
+	}
 
-    render() {
-        return (<h1>Analysis Page</h1>)
-    }
+	componentWillUnmount() {
+		console.log('willUnmount Analysis: 卸载前');
+	}
+
+	render() {
+		return (
+			<h1>Analysis Page</h1>
+		)
+	}
 }
 
